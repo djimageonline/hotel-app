19a8d3938073f151eff162e62e431f4fb0de8b7b
Johnny Proano <jproano22@yahoo.com>
2023-05-17 19:45:34 -0400

    added header and nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<hotel-header></hotel-header>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hotel-app';
+  title = 'HotelR';
 }
