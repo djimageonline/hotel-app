6ade4a9c7af4cfd19e895b11d4592869b80a1206
Johnny Proano <jproano22@yahoo.com>
2023-05-17 21:34:19 -0400

    refactored files and organized in folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './home/header/header.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component'
+import { HeaderComponent } from './home/header/header.component';
+import { HeroComponent } from './home/hero/hero.component'
 
 @NgModule({
   declarations: [
