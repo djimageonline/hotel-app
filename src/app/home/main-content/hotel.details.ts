50f9336cabe32407c6220ca8175d00bdb9fc822d
Johnny Proano <jproano22@yahoo.com>
2023-05-17 22:05:09 -0400

    created hotel.details. and data components

---
+++ src/app/home/main-content/hotel.details.ts
@@ -0,0 +1,61 @@
+
+
+
+
+const hotelDetails : {
+  id: number;
+  name: string;
+  location: string;
+  description: string;
+  photo_url: string;
+  price: number;
+}[] = [
+  {
+  id: 1,
+  name: "Illustrious Veil Motel",
+  location: "Italy",
+  description: "Relaxing views, pools, restaurants and an amazing time",
+  photo_url: "/assets/img/hotel1.jpg",
+  price: 250,
+  },
+  {
+    id: 2,
+    name: "Obsidian Bay Hotel",
+    location: "Hawaii",
+    description: "string",
+    photo_url: "/assets/img/hotel2.jpg",
+    price: 300,
+  },
+  {
+    id: 3,
+    name: "Light Panorama Hotel",
+    location: "Puerto Rico",
+    description: "string",
+    photo_url: "/assets/img/hotel3.jpg",
+    price: 189,
+  },
+  {
+    id: 4,
+    name: "Azure Bazaar Hotel",
+    location: "Rio Viejo",
+    description: "string",
+    photo_url: "/assets/img/hotel4.jpg",
+    price: 275,
+  },
+  {
+    id: 5,
+    name: "Pacific Star Resort & Spa",
+    location: "Blue Point",
+    description: "string",
+    photo_url: "/assets/img/hotel5.jpg",
+    price: 312,
+  },
+  {
+    id: 6,
+    name: "Summer Canopy Hotel",
+    location: "Santa Lucia",
+    description: "string",
+    photo_url: "/assets/img/hotel6.jpg",
+    price: 500,
+  },
+]
