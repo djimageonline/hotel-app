f879cdefd20195ebd1d1941a7fd35a254b21f0b6
Johnny Proano <jproano22@yahoo.com>
2023-05-22 18:49:38 -0400

    added hamburger menu needs style will add by next commit

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'hotel-header',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent {
-} 
+
+  faBars = faBars;
+  isHamdisplayed = false;
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+
+  @HostListener('document:click', ['$event'])
+  handleMenuClick(event: Event): void {
+    const target = event.target as HTMLElement; 
+    console.log("icon clicked", target)
+    this.showNav()
+  }
+
+  showNav() :void {
+    console.log("test nav")
+    const element = this.elementRef.nativeElement.querySelector('#links');
+
+    if (this.isHamdisplayed) {
+      this.renderer.setStyle(element, 'display', 'none');
+    } else {
+      this.renderer.setStyle(element, 'display', 'block');
+    }
+    this.isHamdisplayed = !this.isHamdisplayed;
+  }
+
+
+}
